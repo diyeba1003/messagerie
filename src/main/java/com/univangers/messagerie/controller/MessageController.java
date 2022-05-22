8abc685f7fd02845e7a56bd4f38a7d6efde2ad92
Maty Laye <matylayesouare@gmail.com>
2022-05-22 18:28:11 +0200

    Upload de fichiers

--- src/main/java/com/univangers/messagerie/controller/MessageController.java
+++ src/main/java/com/univangers/messagerie/controller/MessageController.java
@@ -25,6 +25,10 @@ import com.univangers.messagerie.services.PersonneServiceInterface;
 import com.univangers.messagerie.util.Utils;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
@@ -35,10 +39,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 /**
  *
@@ -383,6 +390,32 @@ public class MessageController {
         result.put("messagePerMonth", messagePerMonth);
 
         return result;
+    }
+     @PostMapping("/upload")
+    public String uploadFile(@RequestParam("file") MultipartFile file, RedirectAttributes attributes) {
+
+        // check if file is empty
+        if (file.isEmpty()) {
+            attributes.addFlashAttribute("message", "Please select a file to upload.");
+            return "redirect:/";
+        }
+
+        // normalize the file path
+        String fileName = StringUtils.cleanPath(file.getOriginalFilename());
+        
+
+        // save the file on the local file system
+        try {
+            Path path = Paths.get("/home/etud/NetBeansProjects/messagerie/" + fileName);
+            Files.copy(file.getInputStream(), path, StandardCopyOption.REPLACE_EXISTING);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // return success response
+        attributes.addFlashAttribute("message", "You successfully uploaded " + fileName + '!');
+
+        return "redirect:/";
     }
 
 }
